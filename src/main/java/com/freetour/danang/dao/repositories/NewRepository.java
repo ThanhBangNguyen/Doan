111084f675e0bd79c11168d680704b5fa9bdbea5
ThanhBangNguyen <bangbii.k9tt@gmail.com>
2019-04-24 16:45:28 +0700

    do an TN finish

--- src/main/java/com/freetour/danang/dao/repositories/NewRepository.java
+++ src/main/java/com/freetour/danang/dao/repositories/NewRepository.java
@@ -1,14 +1,14 @@
 package com.freetour.danang.dao.repositories;
 
-import com.freetour.danang.dao.models.Category;
-import com.freetour.danang.dao.models.Menu;
 import com.freetour.danang.dao.models.New;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Random;
 
+@Repository
 public interface NewRepository extends JpaRepository<New, Long> {
 
     @Query(value = "SELECT * FROM new WHERE new_id = 1", nativeQuery = true)
